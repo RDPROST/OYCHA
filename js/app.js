0bcb71abc1ac362ea10d5e978021ca69dd35f8fa
Danila <danila.dvoretskov@yandex.ru>
2022-11-22 01:15:00 +0300

    Commite_3

---
+++ js/app.js
@@ -0,0 +1,19 @@
+document.querySelector('.header__menu-icon').addEventListener('click', (e) => {
+    console.log(1);
+    e.preventDefault();
+    document.querySelector('.header__menu-icon').classList.toggle('header__menu-icon_active');
+})
+let myDiv = true;
+document.querySelector('.header__menu-icon').addEventListener('click', (showDiv) => {
+    let isShow = document.getElementById('header__nav-list');
+    let isPos = document.getElementById('header__menu-icon');
+    if(myDiv){
+        isShow.style.display = 'none';
+        myDiv = false;
+        
+    }
+    else{
+        isShow.style.display = 'flex';
+        myDiv = true;
+    }
+})
--- js/faq.js
+++ js/faq.js
@@ -1,10 +1,15 @@
-document.querySelectorAll('.faq-vector-arrow').forEach((item) =>
-    item.addEventListener('click', () =>{
-        const parent = item.parentNode;
-        parent.classList.toggle('faq-text-span--active');
-        parent.classList.toggle('faq-vector-arrow--active');
-        parent.classList.toggle('panel--active');
-        
-    })
-)
+let acc = document.querySelectorAll(".faq__item");
+      let i;
+
+      for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+          this.classList.toggle("faq__item_active");
+          let panel = this.querySelector(".faq__panel");
+          if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          }
+        });
+      }
 
